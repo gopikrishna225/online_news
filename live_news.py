16639e76355f688db52151cad64363f2ab275f52
gopikrishna225 <gopikrishnaguttapalli@gmail.com>
2022-09-15 11:49:34 +0530

    undo_changes

--- live_news.py
+++ live_news.py
@@ -13,7 +13,6 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 #driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
-# the is made here
 def check_candidate_name(line):
     candidate_names = pd.read_excel(r"/root/Desktop/Team Python Deployment/Gopi Projects/DH_RPA_Validations/DHS_RPA_Validations/online_news/Candidate List.xlsx",sheet_name='Sheet1')
     for i in candidate_names['WINNER CANDIDATE']:
